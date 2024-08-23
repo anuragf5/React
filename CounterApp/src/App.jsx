5f720e3a6261ab153a6f7df131e39429dcd093ac
Anurag <anuragf5@gmail.com>
2024-08-23 10:07:18 +0530

    Button counter app in react

---
+++ CounterApp/src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [count, setCount] = useState(0);
+
+  function increase() {
+    count++;
+    setCount(count);
+  }
+
+  function decrease() {
+    if (count > 0) {
+      count--;
+    }
+    setCount(count);
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={increase}>+</button>
+        <span className="count_text">{count}</span>
+        <button onClick={decrease}>-</button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
