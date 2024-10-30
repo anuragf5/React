e34a0e88378c2cf7d18f776887c06b27b470e5a3
Anurag <anuragf5@gmail.com>
2024-10-30 14:47:11 +0530

    Add functionality of sorting by price and rating

--- E-commerce/src/Components/ShoppingCategory/ShoppingCategory.jsx
+++ E-commerce/src/Components/ShoppingCategory/ShoppingCategory.jsx
@@ -1,6 +1,66 @@
+import { useState } from "react";
 import shopCategoryProductsData from "../DummyData/ShopCategoryProducts";
 
 function ShoppingCategory() {
+  const [selectedPriceValue, setSelectedPriceValue] = useState("");
+  const [selectedRatingValue, setSelectedRatingValue] = useState("");
+
+  const [sortedPirceData, setSortedPriceData] = useState(
+    shopCategoryProductsData
+  );
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setSelectedPriceValue(value);
+
+    let newSortedPriceData;
+
+    if (value === "price_low") {
+      newSortedPriceData = [...shopCategoryProductsData].sort((a, b) => {
+        return Number(a.price) - Number(b.price);
+      });
+      setSelectedRatingValue("default");
+    }
+
+    if (value === "price_high") {
+      newSortedPriceData = [...shopCategoryProductsData].sort((a, b) => {
+        return Number(b.price) - Number(a.price);
+      });
+      setSelectedRatingValue("default");
+    }
+
+    if (value == "default") {
+      newSortedPriceData = shopCategoryProductsData;
+    }
+
+    setSortedPriceData(newSortedPriceData);
+  };
+
+  const handleRatingChange = (event) => {
+    const value = event.target.value;
+    setSelectedRatingValue(value);
+
+    let newSortedRatingData;
+
+    if (value === "rating_low") {
+      newSortedRatingData = [...shopCategoryProductsData].sort((a, b) => {
+        return Number(a.rating) - Number(b.rating);
+      });
+      setSelectedPriceValue("default");
+    }
+    if (value === "rating_high") {
+      newSortedRatingData = [...shopCategoryProductsData].sort((a, b) => {
+        return Number(b.rating) - Number(a.rating);
+      });
+      setSelectedPriceValue("default");
+    }
+    if (value == "default") {
+      newSortedRatingData = shopCategoryProductsData; // Default sorting
+    }
+
+    setSortedPriceData(newSortedRatingData); // Update the displayed data
+  };
+
   return (
     <>
       <div className="cart_box">
@@ -9,13 +69,13 @@ function ShoppingCategory() {
           <p>Very us move be blessed multiply night</p>
         </div>
         <div className="right">
-          <a href="" className="home">
+          <a href="#" className="home">
             Home
           </a>
-          <a href="" className="shop">
+          <a href="#" className="shop">
             Shop
           </a>
-          <a href="" className="women_fashion">
+          <a href="#" className="women_fashion">
             Women Fashion
           </a>
         </div>
@@ -68,7 +128,7 @@ function ShoppingCategory() {
               <h3>Price Filter</h3>
 
               <div className="price_filter_range">
-                <input type="range" />
+                <input type="range" value={0} />
               </div>
 
               <div className="price">
@@ -81,47 +141,72 @@ function ShoppingCategory() {
           <div className="right">
             <div className="product_top_bar">
               <div className="left_product_bar">
-                <select name="" id="">
-                  <option value="">Default sorting</option>
-                  <option value="">Default sorting 01</option>
-                  <option value="">Default sorting 02</option>
+                <select
+                  name=""
+                  id=""
+                  value={selectedPriceValue}
+                  onChange={handleChange}
+                >
+                  <option value="default">Sort By</option>
+                  <option value="price_low">Price -- Low to High</option>
+                  <option value="price_high">Price -- High to Low</option>
                 </select>
               </div>
 
               <div className="right_product_bar">
-                <select name="" id="">
-                  <option value="">Show 12</option>
-                  <option value="">Show 13</option>
-                  <option value="">Show 14</option>
+                <select
+                  name=""
+                  id=""
+                  value={selectedRatingValue}
+                  onChange={handleRatingChange}
+                >
+                  <option value="default">Sort By</option>
+                  <option value="rating_low">Rating -- Low to High</option>
+                  <option value="rating_high">Rating -- High to Low</option>
                 </select>
               </div>
             </div>
 
             <div className="latest_product_container">
               <div className="top_latest_products_container">
-                {shopCategoryProductsData.map((item)=>(
+                {sortedPirceData.map((item) => (
+                  <div className="first_latest_product_container" key={item.id}>
+                    <img
+                      src={item.image}
+                      alt=""
+                      style={{
+                        objectFit: "contain",
+                      }}
+                    />
 
-                <div className="first_latest_product_container">
-                  <img src={item.image} alt="" />
+                    <div className="hidden_buttons">
+                      <span className="material-symbols-outlined">
+                        visibility
+                      </span>
 
-                  <div className="hidden_buttons">
-                    <span className="material-symbols-outlined">
-                      visibility
-                    </span>
+                      <span className="material-symbols-outlined">
+                        favorite
+                      </span>
 
-                    <span className="material-symbols-outlined">favorite</span>
+                      <span className="material-symbols-outlined">
+                        shopping_cart
+                      </span>
+                    </div>
 
-                    <span className="material-symbols-outlined">
-                      shopping_cart
-                    </span>
+                    <div className="inspired_product_text_container">
+                      <h4>{item.title}</h4>
+                      <span style={{ fontWeight: "500", color: "black" }}>
+                        ₹ {item.discountedPrice}
+                        <del style={{ marginLeft: "10px" }}>₹ {item.price}</del>
+                      </span>
+                      <p style={{ marginTop: "16px", fontSize: "16px" }}>
+                        <span style={{ color: "#FFDF00", fontSize: "16px" }}>
+                          <i class="fa-solid fa-star"></i>
+                        </span>
+                        {item.rating}
+                      </p>
+                    </div>
                   </div>
-
-                  <div className="inspired_product_text_container">
-                    <h4>{item.title}</h4>
-                    <span>{item.discountedPrice}</span>
-                    <del>{item.price}</del>
-                  </div>
-                </div>
                 ))}
               </div>
             </div>
