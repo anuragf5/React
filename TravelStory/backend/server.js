9d4c44cda11f57230c9e413843448cbfb0f2cc94
Anurag <anuragf5@gmail.com>
2025-01-18 20:27:39 +0530

    Add path for images

--- TravelStory/backend/server.js
+++ TravelStory/backend/server.js
@@ -4,6 +4,8 @@ const app = express();
 const cors = require("cors");
 const db = require("./config/mongodb");
 const authRoutes = require("./route/authRoute");
+const travelStoryRoutes = require("./route/travelstoryRoute");
+const path = require("path");
 
 dotenv.config();
 db();
@@ -13,6 +15,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRoutes);
+app.use("/travelStory", travelStoryRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 const port = 9090;
 
