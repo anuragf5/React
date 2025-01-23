de1eb659c0d981040777d970b06125331439d874
Anurag <anuragf5@gmail.com>
2025-01-23 23:44:47 +0530

    Add getAllUsers api functionality and call Navbar, TravelStoryCard comopnent in it

--- TravelStory/frontend/src/pages/home/Home.jsx
+++ TravelStory/frontend/src/pages/home/Home.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar";
+import TravelStoryCard from "../../components/cards/TravelStoryCard";
+import { useNavigate } from "react-router-dom";
+import API from "../../utils/api";
 
 const Home = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  const navigate = useNavigate();
+
+  //Get all users
+  const getAllUsers = async () => {
+    try {
+      const response = await API.get("/auth/getAllUsers");
+      console.log(">>>>> All users from Home page <<<<<", response);
+
+      if (response.data.user) {
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      console.log(error);
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
   return (
     <>
-      <p>Home</p>
+      <Navbar />
+      <TravelStoryCard/>
     </>
   );
 };
