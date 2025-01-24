75d02833b93dec167332aab9e184c73a5225e2b3
Anurag <anuragf5@gmail.com>
2025-01-24 22:46:06 +0530

    Change the complete component and remove all the previous functionality

--- TravelStory/frontend/src/components/cards/TravelStoryCard.jsx
+++ TravelStory/frontend/src/components/cards/TravelStoryCard.jsx
@@ -1,57 +1,50 @@
 import React, { useEffect, useState } from "react";
-import API from "../../utils/api";
+import Moment from "moment";
+import { FaHeart } from "react-icons/fa6";
+import { GrMapLocation } from "react-icons/gr";
 
-const TravelStoryCard = () => {
-  const [allTravelStory, setAllTravelStory] = useState([]);
-
-  //Get all travel stories
-  const getAllTravelStories = async () => {
-    const token = localStorage.getItem("token");
-    try {
-      const response = await API.get("/travelStory/getAllStory", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      console.log(
-        ">>>>> All travel stories from Home page <<<<<",
-        response.data.stories
-      );
-
-      setAllTravelStory(response.data.stories); // Set the state with the array if it's valid
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getAllTravelStories();
-  }, []);
-
-  console.log("allTravelStory state:", allTravelStory);
+const TravelStoryCard = ({
+  imgUrl,
+  title,
+  story,
+  date,
+  visitedLocation,
+  isFavorite,
+  onFavoriteClick,
+  onClick,
+}) => {
   return (
     <>
-      <div className="container mx-auto py-10">
-        <div className="flex gap-7">
-          <div className="flex-1">
-            {allTravelStory.length > 0 ? (
-              <div className="grid grid-cols-2 gap-4 p-8">
-                {allTravelStory.map((item) => (
-                  <div key={item._id} className="border p-4 rounded-lg mb-4">
-                    <img src={item.imageUrl} alt="" />
-                    <h3 className="text-xl font-semibold">{item.title}</h3>
-                    <p className="text-gray-700">{item.story}</p>
-                    <p>{item.visitedDate}</p>
-                    <p>{item.visitedLocation}</p>
-                    <p>{item.isFavorite}</p>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <p>No travel stories available.</p>
-            )}
+      <div className="border rounded-lg overflow-hidden bg-white hover:shadow-lg hover:shadow-slate-200 transition-all ease-in-out relative cursor-pointer">
+        <img
+          src={imgUrl}
+          alt={title}
+          className="w-full object-cover rounded-lg"
+        />
+
+        <button
+          className="w-12 h-12 flex items-center justify-center bg-white/40 rounded-lg border border-white/30 absolute top-4 right-4"
+          onClick={onFavoriteClick}
+        >
+          <FaHeart
+            className={`icon-btn ${isFavorite ? "text-red-500" : "text-white"}`}
+          />
+        </button>
+        <div className="p-4" onClick={onClick}>
+          <div className="flex items-center gap-3">
+            <div className="flex-1">
+              <h6 className="text-sm font-medium">{title}</h6>
+              <span className="text-xs text-slate-500">
+                {date ? Moment(date).format("Do MMM YYYY") : "-"}
+              </span>
+            </div>
           </div>
-          <div className="w-[320px]"></div>
+
+          <p className="text-xs text-slate-600 mt-2">{story?.slice(0, 60)}</p>
+          <div className="inline-flex items-center gap-2 text-[13px] text-cyan-600 bg-cyan-200/40 rounded mt-3 px-2 py-1">
+            <GrMapLocation className="text-sm" />
+            {visitedLocation}
+          </div>
         </div>
       </div>
     </>
