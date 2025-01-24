2b58a6f5039ebe0429ab6ada62a77b33cb9b69ff
Anurag <anuragf5@gmail.com>
2025-01-24 22:40:33 +0530

    Create a component for add and edit travel stories

---
+++ TravelStory/frontend/src/pages/home/AddEditTravelStory.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { MdAdd, MdDeleteOutline, MdUpdate, MdClose } from "react-icons/md";
+import DateSelector from "../../components/input/DateSelector";
+
+const AddEditTravelStory = ({
+  storyInfo,
+  type,
+  onClose,
+  getAllTravelStories,
+}) => {
+  const [title, setTitle] = useState("");
+  const [storyImg, setStoryImg] = useState(null);
+  const [story, setStory] = useState(null);
+  const [visitedLocation, setVisitedLocation] = useState([]);
+  const [visitedDate, setVisitedDate] = useState(null);
+  const handleAddOrUpdateClick = () => {};
+
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h5 className="text-xl font-medium text-slate-700">
+          {type === "add" ? "Add Story" : "Update Story"}
+        </h5>
+
+        <div>
+          <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
+            {type === "add" ? (
+              <button className="btn-small" onClick={() => {}}>
+                <MdAdd className="text-lg" />
+                ADD STORY
+              </button>
+            ) : (
+              <>
+                <button className="btn-small" onClick={handleAddOrUpdateClick}>
+                  <MdUpdate className="text-lg" />
+                  UPDATE STORY
+                </button>
+
+                <button className="btn-small btn-delete" onClick={onClose}>
+                  <MdDeleteOutline className="text-lg" />
+                  DELETE
+                </button>
+              </>
+            )}
+
+            <button className="" onClick={onClose}>
+              <MdClose className="text-xl text-scale-400" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex-1 flex flex-col gap-2 pt-4">
+          <label htmlFor="" className="input-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="text-2xl text-slate-950 outline-none"
+            placeholder="A Day at Pushkar Fair"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+          <div className="my-3">
+            <DateSelector date={visitedDate} setDate={setVisitedDate} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddEditTravelStory;
